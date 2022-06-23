7806c92175f31acfb20de02c71b036d69d7ecacd
Ylan Dahan Benoliel Silva <ylan.d@havan.com.br>
2022-06-23 20:21:49 -0300

    Save unique user by email

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
+
+const USER_EMAIL_INDEX = "user_by_email";
 
 export default NextAuth({
   providers: [
@@ -8,4 +12,24 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index(USER_EMAIL_INDEX), q.Casefold(email)))
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index(USER_EMAIL_INDEX), q.Casefold(email)))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
