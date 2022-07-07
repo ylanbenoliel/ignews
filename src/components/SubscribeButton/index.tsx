324542760312d2e225159f6c5fa7a25c3e330acb
Ylan Dahan Benoliel Silva <ylan.d@havan.com.br>
2022-07-06 22:44:35 -0300

    Create subscribe button event

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from "next-auth/react";
+import api from "../../services/api";
+import getStripeJs from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 type SubscribeButtonProps = {
@@ -5,8 +8,33 @@ type SubscribeButtonProps = {
 };
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const { data } = await api.post("/subscribe");
+
+      const { sessionId } = data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribe}>
+    <button
+      type="button"
+      className={styles.subscribe}
+      onClick={() => handleSubscribe()}
+    >
       Subscribe now
     </button>
   );
